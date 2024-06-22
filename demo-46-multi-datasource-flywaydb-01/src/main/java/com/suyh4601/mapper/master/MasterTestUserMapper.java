e6c44d11f63b08d1b0d3642b3c7d7760b423d0e4
suyh <su_787910081@sina.com>
2024-06-22 18:04:23 +0800

    first init

---
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/mapper/master/MasterTestUserMapper.java
@@ -0,0 +1,9 @@
+package com.suyh4601.mapper.master;
+
+import com.suyh4601.entity.master.MasterTestUser;
+
+import java.util.List;
+
+public interface MasterTestUserMapper {
+    List<MasterTestUser> selectAll();
+}
